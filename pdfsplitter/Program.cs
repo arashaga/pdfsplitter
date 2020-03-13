2002e3fb7455792ac2c61486e24eb3956e7eacf5
Arash Mosharraf <armoshar@microsoft.com>
2020-03-13 12:40:59 -0400

    Add project files.

---
+++ pdfsplitter/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace pdfsplitter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
